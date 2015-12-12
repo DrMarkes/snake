658aecffe3b02a91015de7447f2c2efa4bb5676a
DrMarkes <markovandrew@yandex.ru>
2015-12-12 23:25:57 +0700

    создан класс змейка

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -12,15 +12,20 @@ namespace Snake
         {
             Console.SetBufferSize(80, 25);
 
+            //Рамка игры
             HorizontalLine topLine = new HorizontalLine(0, 78, 0, '+');
             HorizontalLine bottomLine = new HorizontalLine(0, 78, 24, '+');
             VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
             VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-
             topLine.Draw();
             bottomLine.Draw();
             leftLine.Draw();
             rightLine.Draw();
+
+            Point p = new Point(3, 7, '*');
+
+            Snake snake = new Snake(p, 10, Direction.DOWN);
+            snake.Draw();
 
             Console.ReadLine();
         }
